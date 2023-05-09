153cbfb404180783ddba3c866f34b2207d2e8fe9
KateMwende <kmwende419@gmail.com>
2023-05-09 11:43:18 +0000

    Deployment settings

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#script that sets up your web servers for the deployment of web_static
+
+sudo apt -y update
+sudo apt -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+sudo echo “
+location /hbnb_static {
+    alias /data/web_static/current;
+    index index.html index.htm;
+}” >> /etc/nginx/sites-available
+sudo service nginx restart
